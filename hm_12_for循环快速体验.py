ccfbace761a57500325ca25099820c01a8fe21c3
wiserhzc <1736835823@qq.com>
2020-10-13 21:49:59 +0800

    1

---
+++ hm_12_for循环快速体验.py
@@ -0,0 +1,24 @@
+"""
+for 临时变量 in 序列:
+    重复执行的代码
+    ......
+"""
+
+"""
+1. 准备一个数据序列
+2. for
+"""
+
+str1 = 'itheima'
+for i in str1:
+    print(i)
+num = [1, 2, 3, 4, 5, 6, 7]
+for j in num:
+    if j <= 5:
+        print(j+1)
+    print(j*j)
+
+
+
+
+
